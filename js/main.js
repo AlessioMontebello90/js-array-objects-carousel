abc479cb3085e64b998daa277ffce68b1824b2a1
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-08-25 17:49:01 +0200

    fix js obj

--- js/main.js
+++ js/main.js
@@ -1,28 +1,33 @@
-const carouselContainer = document.querySelector("slider");
+const carouselImg = document.querySelector("slider");
 const images = [
   {
-    url: "./img/01.webp",
-    title: "",
-    description: "Descrizione 1",
+    images: "./img/01.webp",
+    title: "Marvel's Spiderman Miles Morale",
+    description:
+      "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man",
   },
   {
-    url: "./img/02.webp",
-    title: "",
-    description: "Descrizione 2",
+    images: "./img/02.webp",
+    title: "Ratchet & Clank: Rift Apart",
+    description:
+      "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
   },
   {
-    url: "./img/03.webp",
-    title: "",
-    description: "Descrizione 3",
+    images: "./img/03.webp",
+    title: "Fortnite",
+    description:
+      "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
   },
   {
-    url: "./img/04.webp",
-    title: "",
-    description: "Descrizione 4",
+    images: "./img/04.webp",
+    title: "Stray",
+    description:
+      "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
   },
   {
-    url: "./img/05.webp",
-    title: "",
-    description: "Descrizione 5",
+    images: "./img/05.webp",
+    title: "Marvel's Avengers",
+    description:
+      "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
   },
 ];
