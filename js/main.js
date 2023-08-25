6494d978390a6a339493b8e1edb3f5c6516239ae
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-08-25 17:11:18 +0200

    add obj  in Js

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const carouselContainer = document.querySelector("slider");
+const images = [
+  {
+    url: "./img/01.webp",
+    title: "",
+    description: "Descrizione 1",
+  },
+  {
+    url: "./img/02.webp",
+    title: "",
+    description: "Descrizione 2",
+  },
+  {
+    url: "./img/03.webp",
+    title: "",
+    description: "Descrizione 3",
+  },
+  {
+    url: "./img/04.webp",
+    title: "",
+    description: "Descrizione 4",
+  },
+  {
+    url: "./img/05.webp",
+    title: "",
+    description: "Descrizione 5",
+  },
+];
