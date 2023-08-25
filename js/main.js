51e791cf562117964d81280a1bb4fcf1f442a7c6
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-08-25 16:20:48 +0200

    add object JS

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// script.js
+const carouselContainer = document.querySelector(".carousel-container");
+const images = [
+  {
+    url: "./img/01.webp",
+    title: "Titolo 1",
+    description: "Marvel's Spiderman Miles Morales",
+  },
+  {
+    url: "./img/02.webp",
+    title: "Titolo 1",
+  },
+  {
+    url: "./img/03.webp",
+    title: "Titolo 1",
+  },
+  {
+    url: "./img/04.webp",
+    title: "Titolo 1",
+  },
+  {
+    url: "./img/05.webp",
+    title: "Titolo 1",
+  },
+];
